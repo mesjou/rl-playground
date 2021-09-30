76e96e5ef543f6ec7585f5f621d7b7b22fe73307
matthias-hettich <matthias.hettich@outlook.de>
2021-09-30 14:01:22 +0200

    set up structure

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # rl-playground
+
+## Get started
+
+Prerequisites (Example for MacOS only):
+
+* macOS
+* pyenv installed
+
+Clone the repo and install requirements with the correct pypthon version
+```bash
+git clone https://github.com/mesjou/rl-playground.git && cd rl-playground
+
+pyenv install 3.8.10
+pyenv virtualenv 3.8.10 rl-project
+pyenv local rl-project
+
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt 
+```
+
+
+## References
+
+I have been heavily relying on the `cleanrl` repo:
+* https://github.com/vwxyzjn/cleanrl
---
+++ agents/ppo.py
@@ -0,0 +1,350 @@
+import argparse
+import os
+import random
+import time
+from distutils.util import strtobool
+
+import gym
+import numpy as np
+import tensorflow as tf
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.distributions.categorical import Categorical
+from torch.utils.tensorboard import SummaryWriter
+from tensorflow.keras.optimizers import Adam
+
+
+def parse_args():
+    # fmt: off
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--exp-name', type=str, default=os.path.basename(__file__).rstrip(".py"),
+        help='the name of this experiment')
+    parser.add_argument('--gym-id', type=str, default="CartPole-v1",
+        help='the id of the gym environment')
+    parser.add_argument('--learning-rate', type=float, default=2.5e-4,
+        help='the learning rate of the optimizer')
+    parser.add_argument('--seed', type=int, default=1,
+        help='seed of the experiment')
+    parser.add_argument('--total-timesteps', type=int, default=25000,
+        help='total timesteps of the experiments')
+    parser.add_argument('--torch-deterministic', type=lambda x:bool(strtobool(x)), default=True, nargs='?', const=True,
+        help='if toggled, `torch.backends.cudnn.deterministic=False`')
+    parser.add_argument('--cuda', type=lambda x:bool(strtobool(x)), default=True, nargs='?', const=True,
+        help='if toggled, cuda will be enabled by default')
+    parser.add_argument('--track', type=lambda x:bool(strtobool(x)), default=False, nargs='?', const=True,
+        help='if toggled, this experiment will be tracked with Weights and Biases')
+    parser.add_argument('--wandb-project-name', type=str, default="cleanRL",
+        help="the wandb's project name")
+    parser.add_argument('--wandb-entity', type=str, default=None,
+        help="the entity (team) of wandb's project")
+    parser.add_argument('--capture-video', type=lambda x:bool(strtobool(x)), default=False, nargs='?', const=True,
+        help='weather to capture videos of the agent performances (check out `videos` folder)')
+
+    # Algorithm specific arguments
+    parser.add_argument('--num-envs', type=int, default=4,
+        help='the number of parallel game environments')
+    parser.add_argument('--num-steps', type=int, default=128,
+        help='the number of steps to run in each environment per policy rollout')
+    parser.add_argument('--anneal-lr', type=lambda x:bool(strtobool(x)), default=False, nargs='?', const=True, # todo cahnge default to true
+        help="Toggle learning rate annealing for policy and value networks")
+    parser.add_argument('--gae', type=lambda x:bool(strtobool(x)), default=True, nargs='?', const=True,
+        help='Use GAE for advantage computation')
+    parser.add_argument('--gamma', type=float, default=0.99,
+        help='the discount factor gamma')
+    parser.add_argument('--gae-lambda', type=float, default=0.95,
+        help='the lambda for the general advantage estimation')
+    parser.add_argument('--num-minibatches', type=int, default=4,
+        help='the number of mini-batches')
+    parser.add_argument('--update-epochs', type=int, default=4,
+        help="the K epochs to update the policy")
+    parser.add_argument('--norm-adv', type=lambda x:bool(strtobool(x)), default=True, nargs='?', const=True,
+        help="Toggles advantages normalization")
+    parser.add_argument('--clip-coef', type=float, default=0.2,
+        help="the surrogate clipping coefficient")
+    parser.add_argument('--clip-vloss', type=lambda x:bool(strtobool(x)), default=True, nargs='?', const=True,
+        help='Toggles wheter or not to use a clipped loss for the value function, as per the paper.')
+    parser.add_argument('--ent-coef', type=float, default=0.01,
+        help="coefficient of the entropy")
+    parser.add_argument('--vf-coef', type=float, default=0.5,
+        help="coefficient of the value function")
+    parser.add_argument('--max-grad-norm', type=float, default=0.5,
+        help='the maximum norm for the gradient clipping')
+    parser.add_argument('--target-kl', type=float, default=None,
+        help='the target KL divergence threshold')
+    args = parser.parse_args()
+    args.batch_size = int(args.num_envs * args.num_steps)
+    args.minibatch_size = int(args.batch_size // args.num_minibatches)
+    # fmt: on
+    return args
+
+
+def make_env(gym_id, seed, idx, capture_video, run_name):
+    def thunk():
+        env = gym.make(gym_id)
+        env = gym.wrappers.RecordEpisodeStatistics(env)
+        if capture_video:
+            if idx == 0:
+                env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
+        env.seed(seed)
+        env.action_space.seed(seed)
+        env.observation_space.seed(seed)
+        return env
+
+    return thunk
+
+
+def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
+    torch.nn.init.orthogonal_(layer.weight, std)
+    torch.nn.init.constant_(layer.bias, bias_const)
+    return layer
+
+
+class Agent(tf.keras.Model):
+    def __init__(self, envs):
+        super().__init__()
+
+        self.act_size = envs.single_action_space.n
+
+        # critic_network
+        inputs = tf.keras.layers.Input(shape=(int(np.product(envs.single_observation_space.shape)),), name="observations")
+        first_layer = tf.keras.layers.Dense(64, name="1st_critic_layer", activation='tanh')(inputs)
+        second_layer = tf.keras.layers.Dense(64, name="2nd_critic_layer", activation='tanh')(first_layer)
+        value_out = tf.keras.layers.Dense(1, name="critic_value")(second_layer)
+
+        # actor network
+        first_layer = tf.keras.layers.Dense(64, name="1st_actor_layer", activation='tanh')(inputs)
+        second_layer = tf.keras.layers.Dense(64, name="2nd_actor_layer", activation='tanh')(first_layer)
+        logits_out = tf.keras.layers.Dense(self.act_size, activation=None, name="action_logits")(second_layer)
+
+        self.base_model = tf.keras.Model(inputs, [logits_out, value_out])
+
+    def call(self, inputs):
+        logits, values = self.base_model(inputs)
+        return logits, values
+
+    def get_value(self, x):
+        return self.critic(x)
+
+    def logp(self, logits, action):
+        """
+        Returns:
+            logp based on the action drawn from prob-distribution \n
+            indexes in the logp_all with one_hot
+        """
+        logp_all = tf.nn.log_softmax(logits)
+        one_hot = tf.one_hot(action, depth=self.act_size)
+        logp = tf.reduce_sum(one_hot * logp_all, axis=-1)
+        return logp
+
+    def entropy(self, logits=None):
+        """
+            Entropy term for more randomness which means more exploration \n
+            Based on OpenAI Baselines implementation
+        """
+        a0 = logits - tf.reduce_max(logits, axis=-1, keepdims=True)
+        exp_a0 = tf.exp(a0)
+        z0 = tf.reduce_sum(exp_a0, axis=-1, keepdims=True)
+        p0 = exp_a0 / z0
+        entropy = tf.reduce_sum(p0 * (tf.math.log(z0) - a0), axis=-1)
+        return entropy
+
+    def get_action_and_value(self, obs, actions=None):
+        logits, values = self.predict(obs)
+        if actions is None:
+            actions = tf.squeeze(tf.random.categorical(logits, 1), axis=-1)
+        return actions, self.logp(logits, actions), self.entropy(logits), values
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    run_name = f"{args.gym_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
+    if args.track:
+        import wandb
+
+        wandb.init(
+            project=args.wandb_project_name,
+            entity=args.wandb_entity,
+            sync_tensorboard=True,
+            config=vars(args),
+            name=run_name,
+            monitor_gym=True,
+            save_code=True,
+        )
+    writer = SummaryWriter(f"runs/{run_name}")
+    writer.add_text(
+        "hyperparameters",
+        "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
+    )
+
+    # TRY NOT TO MODIFY: seeding
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    tf.random.set_seed(args.seed)
+
+    # env setup
+    envs = gym.vector.SyncVectorEnv(
+        [make_env(args.gym_id, args.seed + i, i, args.capture_video, run_name) for i in range(args.num_envs)]
+    )
+    assert isinstance(envs.single_action_space, gym.spaces.Discrete), "only discrete action space is supported"
+
+    agent = Agent(envs)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=args.learning_rate, epsilon=1e-5)
+
+    # ALGO Logic: Storage setup
+    obs = []
+    actions = []
+    logprobs = []
+    rewards = []
+    dones = []
+    values = []
+
+
+    obs = tf.zeros((args.num_steps, args.num_envs) + envs.single_observation_space.shape)
+    actions = tf.zeros((args.num_steps, args.num_envs) + envs.single_action_space.shape)
+    logprobs = tf.zeros((args.num_steps, args.num_envs))
+    rewards = tf.zeros((args.num_steps, args.num_envs))
+    dones = tf.zeros((args.num_steps, args.num_envs))
+    values = tf.zeros((args.num_steps, args.num_envs))
+
+    # TRY NOT TO MODIFY: start the game
+    global_step = 0
+    start_time = time.time()
+    next_obs = envs.reset()
+    done = np.zeros(args.num_envs)
+    num_updates = args.total_timesteps // args.batch_size
+
+    for update in range(1, num_updates + 1):
+        # Annealing the rate if instructed to do so.
+        if args.anneal_lr:
+            frac = 1.0 - (update - 1.0) / num_updates
+            lrnow = frac * args.learning_rate
+            optimizer.param_groups[0]["lr"] = lrnow
+
+        for step in range(0, args.num_steps):
+            global_step += 1 * args.num_envs
+            obs.append(next_obs)
+            dones.append(done)
+
+            # ALGO LOGIC: action logic
+            action, logprob, _, value = agent.get_action_and_value(next_obs)
+            values.append(value.flatten())
+            actions.append(action)
+            logprobs.append(logprob)
+
+            # TRY NOT TO MODIFY: execute the game and log data.
+            next_obs, reward, done, info = envs.step(action.cpu().numpy())
+            rewards.append(reward)
+
+            for item in info:
+                if "episode" in item.keys():
+                    print(f"global_step={global_step}, episodic_return={item['episode']['r']}")
+                    writer.add_scalar("charts/episodic_return", item["episode"]["r"], global_step)
+                    writer.add_scalar("charts/episodic_length", item["episode"]["l"], global_step)
+                    break
+
+        # convert data to numpy array
+        obs = np.array(obs, dtype=np.float32)
+        actions = np.array(actions, dtype=np.int32)
+        logprobs = np.array(logprobs, dtype=np.float32)
+        rewards = np.array(rewards, dtype=np.float32)
+        dones = np.array(dones, dtype=np.bool)
+        values = np.array(values, dtype=np.float32)
+
+        # bootstrap value if not done
+        _, _, _, next_value = agent.agent.get_action_and_value(next_obs)
+        advantages = np.zeros_like(rewards)
+        lastgaelam = 0
+        for t in reversed(range(args.num_steps)):
+            if t == args.num_steps - 1:
+                nextnonterminal = 1.0 - done
+                nextvalues = next_value
+            else:
+                nextnonterminal = 1.0 - dones[t + 1]
+                nextvalues = values[t + 1]
+            delta = rewards[t] + args.gamma * nextvalues * nextnonterminal - values[t]
+            advantages[t] = lastgaelam = delta + args.gamma * args.gae_lambda * nextnonterminal * lastgaelam
+
+        returns = advantages + values
+        # todo normalize advantages?
+
+        # flatten the batch
+        b_obs = obs.reshape((-1,) + envs.single_observation_space.shape)
+        b_logprobs = logprobs.reshape(-1)
+        b_actions = actions.reshape((-1,) + envs.single_action_space.shape)
+        b_advantages = advantages.reshape(-1)
+        b_returns = returns.reshape(-1)
+        b_values = values.reshape(-1)
+
+        # Optimizaing the policy and value network
+        b_inds = np.arange(args.batch_size)
+        clipfracs = []
+        for epoch in range(args.update_epochs):
+            np.random.shuffle(b_inds)
+            for start in range(0, args.batch_size, args.minibatch_size):
+                end = start + args.minibatch_size
+                mb_inds = b_inds[start:end]
+
+                _, newlogprob, entropy, newvalue = agent.get_action_and_value(b_obs[mb_inds], b_actions.long()[mb_inds])
+                logratio = newlogprob - b_logprobs[mb_inds]
+                ratio = logratio.exp()
+
+                with torch.no_grad():
+                    # calculate approx_kl http://joschu.net/blog/kl-approx.html
+                    # old_approx_kl = (-logratio).mean()
+                    approx_kl = ((ratio - 1) - logratio).mean()
+                    clipfracs += [((ratio - 1.0).abs() > args.clip_coef).float().mean().item()]
+
+                mb_advantages = b_advantages[mb_inds]
+                if args.norm_adv:
+                    mb_advantages = (mb_advantages - mb_advantages.mean()) / (mb_advantages.std() + 1e-8)
+
+                # Policy loss
+                pg_loss1 = -mb_advantages * ratio
+                pg_loss2 = -mb_advantages * torch.clamp(ratio, 1 - args.clip_coef, 1 + args.clip_coef)
+                pg_loss = torch.max(pg_loss1, pg_loss2).mean()
+
+                # Value loss
+                newvalue = newvalue.view(-1)
+                if args.clip_vloss:
+                    v_loss_unclipped = (newvalue - b_returns[mb_inds]) ** 2
+                    v_clipped = b_values[mb_inds] + torch.clamp(
+                        newvalue - b_values[mb_inds],
+                        -args.clip_coef,
+                        args.clip_coef,
+                    )
+                    v_loss_clipped = (v_clipped - b_returns[mb_inds]) ** 2
+                    v_loss_max = torch.max(v_loss_unclipped, v_loss_clipped)
+                    v_loss = 0.5 * v_loss_max.mean()
+                else:
+                    v_loss = 0.5 * ((newvalue - b_returns[mb_inds]) ** 2).mean()
+
+                entropy_loss = entropy.mean()
+                loss = pg_loss - args.ent_coef * entropy_loss + v_loss * args.vf_coef
+
+                optimizer.zero_grad()
+                loss.backward()
+                nn.utils.clip_grad_norm_(agent.parameters(), args.max_grad_norm)
+                optimizer.step()
+
+            if args.target_kl is not None:
+                if approx_kl > args.target_kl:
+                    break
+
+        y_pred, y_true = b_values.cpu().numpy(), b_returns.cpu().numpy()
+        var_y = np.var(y_true)
+        explained_var = np.nan if var_y == 0 else 1 - np.var(y_true - y_pred) / var_y
+
+        # TRY NOT TO MODIFY: record rewards for plotting purposes
+        writer.add_scalar("charts/learning_rate", optimizer.param_groups[0]["lr"], global_step)
+        writer.add_scalar("losses/value_loss", v_loss.item(), global_step)
+        writer.add_scalar("losses/policy_loss", pg_loss.item(), global_step)
+        writer.add_scalar("losses/entropy", entropy_loss.item(), global_step)
+        writer.add_scalar("losses/approx_kl", approx_kl.item(), global_step)
+        writer.add_scalar("losses/clipfrac", np.mean(clipfracs), global_step)
+        writer.add_scalar("losses/explained_variance", explained_var, global_step)
+        print("SPS:", int(global_step / (time.time() - start_time)))
+        writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
+
+    envs.close()
+    writer.close()
