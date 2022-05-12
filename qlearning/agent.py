17762e623f6aef5c7bb1a85f62b73a508264e099
mesjou <matthias.hettich@outlook.de>
2022-05-12 13:41:14 +0200

    reformatted and added typing

--- qlearning/agent.py
+++ qlearning/agent.py
@@ -1,14 +1,16 @@
+from typing import Tuple
+
 import numpy as np
 
 
 class QLearnAgent():
-    def __init__(self, n_actions, n_states):
-            self.n_actions = n_actions
-            self.n_states = n_states
-            self.qtable = np.random.uniform(low=-1, high=1, size = self.n_states + (self.n_actions,) )
+    def __init__(self, n_actions: int, n_states: Tuple):
+        self.n_actions = n_actions
+        self.n_states = n_states
+        self.qtable = np.random.uniform(low=-1, high=1, size = self.n_states + (self.n_actions,) )
 
-
-    def maxaction(self,next_state):  # if there are actions with equal values a random action of those will be choosen
+    def maxaction(self, next_state: np.array) -> int:
+        """return action with highest q-value. If values equal choose randomly."""
         tuple_next_state = tuple(obj for obj in next_state)
         smallq = self.qtable[tuple_next_state]
         listmaxactions = np.argwhere(smallq == np.amax(smallq))
@@ -18,15 +20,16 @@ class QLearnAgent():
             listmaxactions2 = np.array([ob[0] for ob in listmaxactions])
             return np.random.choice(listmaxactions2)
 
-    def act(self,next_state,epsilon = 0):# epsilon greedy policy
+    def act(self, next_state: np.array, epsilon: float = 0) -> int:
+        """epsilon greedy policy"""
         if np.random.random() > epsilon:
             self.action = self.maxaction(next_state)
         else:
             self.action = np.random.choice(self.n_actions)  # take a random action
         return self.action
 
-
-    def learn(self, action, reward, state, next_state, done, alpha = 0.8, gamma = 0.9):#qlearning
-                oldq = self.qtable[state[0]][state[1]][action]
-                newq = round(oldq + ( alpha * (1- done) * (reward + gamma * np.max(self.qtable[next_state[0]][next_state[1]]) - oldq)), 4)
-                self.qtable[state[0]][state[1]][action] = newq
+    def learn(self, action, reward, state, next_state, done, alpha = 0.8, gamma: float = 0.9):
+        """update policy via q-learning"""
+        oldq = self.qtable[state[0]][state[1]][action]
+        newq = round(oldq + (alpha * (1.0 - done) * (reward + gamma * np.max(self.qtable[next_state[0]][next_state[1]]) - oldq)), 4)
+        self.qtable[state[0]][state[1]][action] = newq
